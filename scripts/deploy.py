ea347d89b8e85114d673759f61b0793854e5de4d
0xKalakaua <0xKalakaua@gmail.com>
2022-03-28 18:00:52 +0200

    Rinkeby deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,13 +6,14 @@ def main():
     work = accounts.load("work")
     print(network.show_active())
     publish_source = True # Not supported on Testnet
-    xrlc = "0x6F849B9781f4ee13B983f5E5b1485786A4D411d5"
+    xrlc = "0x7b8Ca0e3A88d3BaEC8Cb8781e9Ca83706315e0DD"
     name = "FTL Artifacts"
     symbol = "ARTIFACT"
     uri = "my_uri/"
     base_price = "122 ether"
     max_price = "222 ether"
-    price_increase_factor = "0.08196 ether"
+    # price_increase_factor = "0.08196 ether"
+    price_increase_factor = "0.827 ether"
     admin = work
     Artifacts.deploy(
                 name,
